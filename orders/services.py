158b104ff83849164c09cf3cf26d288c9cf133c3
Sajjad Hussain <sajjadast786@gmail.com>
2024-03-26 02:24:54 +0500

    14

--- orders/services.py
+++ orders/services.py
@@ -1,6 +1,5 @@
 # services.py
 
-from fastapi import APIRouter, HTTPException, Header,Response
 from .schemas import Order
 from db.connection import conn
 import hmac
@@ -15,23 +14,24 @@ def verify_shopify_webhook(order: dict, hmac_header: str) :
     calculated_hmac = hmac.new(shared_secret_key.encode('utf-8'), str(order).encode('utf-8'), hashlib.sha256).hexdigest()
     return hmac.compare_digest(calculated_hmac, hmac_header)
 
-def save_order(order_data, hmac_header):
-    #order_json = json.dumps(order_data)
-    #formatted_data_string = "'" + order_json + "'"
+def save_order(orderDict):
 
-    query = """
-    INSERT INTO orders (order_id, customer_id, total_price)
-    VALUES (%s, %s, %s)
-    RETURNING order_id, customer_id, total_price
-    """
-    values = ('formatted_data_string', "88", 99)
-    with conn.cursor() as cur:
-        cur.execute(query, values)
-        saved_order = cur.fetchone()
-        conn.commit()
-        conn.close()
+    try:
+        query = """
+        INSERT INTO orders (order_id, customer_id, total_price)
+        VALUES (%s, %s, %s)
+        RETURNING order_id, customer_id, total_price
+        """
+        values = ("aaaaa", "88", 99)
+        with conn.cursor() as cur:
+            cur.execute(query, values)
+            saved_order = cur.fetchone()
+            conn.commit()
+            conn.close()
 
-    return {"oder_id":""}
+            return {"status_code": 200, "data": "", "message": "Order saved successfully"}
+    except psycopg2.Error as e:
+        return {"status_code": 400, "data": None, "message": str(e)}
     
     
 
