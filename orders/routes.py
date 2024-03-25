04db5ffe759e5938a9fc4ee6a62244c7fe828632
Sajjad Hussain <sajjadast786@gmail.com>
2024-03-26 01:10:04 +0500

    14

--- orders/routes.py
+++ orders/routes.py
@@ -1,6 +1,6 @@
 # routes.py
 
-from fastapi import APIRouter, HTTPException, Header,Response
+from fastapi import APIRouter, HTTPException, Header,Response,Request
 from .schemas import Order
 from .services import verify_shopify_webhook, save_order, get_orders
 from fastapi import Header
@@ -11,24 +11,11 @@ router = APIRouter()
 
 
 @router.post("/webhooks/orders/create")
-async def handle_order_creation_webhook(
-    payload: dict,
-    X_Shopify_Hmac_Sha256: str = Header(None, convert_underscores=False)
-):
+async def handle_webhook(request: Request):
+    data = await request.body()
+    hmac_header = request.headers.get('X-Shopify-Hmac-SHA256')
     
-    if X_Shopify_Hmac_Sha256 == None:
-        raise HTTPException(status_code=404, detail="Missing 'X-Shopify-Hmac-SHA256' header",
-        headers={"X-Error": "There goes my error"}                    
-                            
-                            )
-
-    if not verify_shopify_webhook(payload, X_Shopify_Hmac_Sha256):
-        payload={"message":"Invalid Shopify webhook"}
-        raise HTTPException(status_code=401, detail="Invalid Shopify webhook")
-
-    return save_order(payload)
-    
-
+    Response("",status_code=200)
 
 @router.get("/orders")
 async def get_orders_route():
