5498fae80c15a38260a5776d02f965606fc04509
Sajjad Hussain <sajjadast786@gmail.com>
2024-03-25 20:14:15 +0500

    14

--- orders/routes.py
+++ orders/routes.py
@@ -5,16 +5,19 @@ from .schemas import Order
 from .services import verify_shopify_webhook, save_order, get_orders
 from fastapi import Header
 import json
+from pydantic import BaseModel
 
 router = APIRouter()
 
+class OrderPayload(BaseModel):
+    id: str
+
 @router.post("/webhooks/orders/create")
 async def create_order_webhook(
-    **kwargs):
-    arguments_json = json.dumps(kwargs)
+    payload: OrderPayload):
 
-
-    return save_order(arguments_json)
+    return {"message":"OK"}
+    #return save_order(arguments_json)
 
 
 @router.get("/orders")
