af935abee7d52676c4c93d6b5267a7616116091e
Sajjad Hussain <sajjadast786@gmail.com>
2024-03-26 23:38:52 +0500

    14

--- orders/routes.py
+++ orders/routes.py
@@ -15,24 +15,18 @@ CLIENT_SECRET = "c72c3df7dcb6c38b68a9e8cf225aec964e5febed202dc025873691cf085d5eb
 router = APIRouter()
 
 
-def verify_webhook(data, hmac_header):
-    digest = hmac.new(CLIENT_SECRET.encode('utf-8'), data, digestmod=hashlib.sha256).digest()
-    computed_hmac = base64.b64encode(digest)
-
-    return hmac.compare_digest(computed_hmac, hmac_header.encode('utf-8'))
-
 
 @router.post("/webhooks/orders/create")
 async def handle_webhook(request: Request):
     try:
         data = await request.body()
-        verified = verify_webhook(data, request.headers.get('X-Shopify-Hmac-SHA256'))
+        verified = verify_shopify_webhook(data, request.headers.get('X-Shopify-Hmac-SHA256'))
 
-        # Save order to database
-        save_order(str(verified),str(verified),str(verified))
-
-            # Respond with success
-        return {"message": 200}
+        if verified:
+            save_order(data)
+            return Response(content="OK", status_code=200)
+        else:    
+            raise HTTPException(status_code=401, detail="Unauthorized! The HMACs do not match. Verification failed.")
 
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal server error")
--- orders/services.py
+++ orders/services.py
@@ -9,33 +9,58 @@ import json
 import base64
 
 
-shared_secret_key = "c72c3df7dcb6c38b68a9e8cf225aec964e5febed202dc025873691cf085d5eb9"
+CLIENT_SECRET = "c72c3df7dcb6c38b68a9e8cf225aec964e5febed202dc025873691cf085d5eb9"
 
 def calculated_hmac(order):
     # Calculate HMAC
-    calculated_hmac = hmac.new(shared_secret_key.encode('utf-8'), order.encode('utf-8'), hashlib.sha256).digest()
+    calculated_hmac = hmac.new(CLIENT_SECRET.encode('utf-8'), order.encode('utf-8'), hashlib.sha256).digest()
     # Base 64 encoding
     calculated_hmac_base64 = base64.b64encode(calculated_hmac).decode('utf-8')
     return calculated_hmac_base64
 
-def verify_shopify_webhook(order, hmac_header) :
-    calculated_hmac = hmac.new(shared_secret_key.encode('utf-8'), str(order).encode('utf-8'), hashlib.sha256).hexdigest()
-    return hmac.compare_digest(calculated_hmac, hmac_header)
+def verify_shopify_webhook(data, hmac_header) :
+    digest = hmac.new(CLIENT_SECRET.encode('utf-8'), data, digestmod=hashlib.sha256).digest()
+    computed_hmac = base64.b64encode(digest)
 
-def save_order(data_string,hmac_header,generated_hmac):
+    return hmac.compare_digest(computed_hmac, hmac_header.encode('utf-8'))
+
+def save_order(data):
+    # Save order to database
+    request_data = data.decode('utf-8')
+
+    # Parse JSON data
+    order_json_data = json.loads(request_data)
 
     try:
         query = """
-        INSERT INTO orders (data, header_hmac, generated_hmac)
-        VALUES (%s, %s, %s)
-        RETURNING data, header_hmac, generated_hmac
+        INSERT INTO orders (id, created_at, currency, current_total_price, total_tax, total_discounts, customer_locale, financial_status, fulfillment_status, order_status_url, line_items, tax_lines, shipping_address, customer)
+        VALUES (%(id)s, %(created_at)s, %(currency)s, %(current_total_price)s, %(total_tax)s, %(total_discounts)s, %(customer_locale)s, %(financial_status)s, %(fulfillment_status)s, %(order_status_url)s, %(line_items)s, %(tax_lines)s, %(shipping_address)s, %(customer)s)
+        RETURNING id
         """
-        values = (data_string, hmac_header, generated_hmac)
+
+        # Adjust the values
+        values = {
+            'id': order_json_data['id'],
+            'created_at': order_json_data['created_at'],
+            'currency': order_json_data['currency'],
+            'current_total_price': order_json_data['current_total_price'],
+            'total_tax': order_json_data['total_tax'],
+            'total_discounts': order_json_data['total_discounts'],
+            'customer_locale': order_json_data['customer_locale'],
+            'financial_status': order_json_data['financial_status'],
+            'fulfillment_status': order_json_data['fulfillment_status'],
+            'order_status_url': order_json_data['order_status_url'],
+            'line_items': json.dumps(order_json_data['line_items']),
+            'tax_lines': json.dumps(order_json_data['tax_lines']),
+            'shipping_address': json.dumps(order_json_data['shipping_address']),
+            'customer': json.dumps(order_json_data['customer'])
+        }
+
+        # Execute the query
         with conn.cursor() as cur:
             cur.execute(query, values)
-            saved_order = cur.fetchone()
+            saved_order_id = cur.fetchone()  # Fetching the ID of the inserted order
             conn.commit()
-            conn.close()
 
             return {"status_code": 200, "data": "", "message": "Order saved successfully"}
     except psycopg2.Error as e:
